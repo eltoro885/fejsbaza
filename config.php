d16b74b65181aabaca16590c56cd46454b6cc2d4
eltoro885 <charlieserbia@gmail.com>
2016-07-22 15:27:07 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Facebook Application ID
+define('FACEBOOK_APP_ID', '1015007118548511');
+
+// Facebook Application Secret
+define('FACEBOOK_APP_SECRET', 'd8dfab63e8756cfdb14fd60483cb7bc2');
